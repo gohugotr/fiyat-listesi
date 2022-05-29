0df97a5071cf13cdd2e901de59206ec84a285f9b
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-29 16:23:03 +0300

    Deploy hazırlık işlemleri yapıldı

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    PUBLIC_URL: 'https://gohugotr.github.io/fiyat-listesi',
+    assetPrefix: './',
+  },
 }
 
 module.exports = nextConfig
+
