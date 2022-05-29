0934af44cd785aa79eed1c79d8e626ee93f90fab
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-29 19:21:08 +0300

    ..

--- next.config.js
+++ next.config.js
@@ -6,11 +6,6 @@ const nextConfig = {
     path: '',
   },
   trailingSlash: true,
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-    }
-  },
 }
 
 module.exports = nextConfig
