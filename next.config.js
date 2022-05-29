d7204f643c520f7236842b1a49260322d68d143f
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-29 18:02:36 +0300

    Düzeltmeler

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
     loader: 'akamai',
     path: '',
   },
+  env: {
+    PUBLIC_URL: 'https://gohugotr.github.io/fiyat-listesi/',
+    assetPrefix: './',
+  },
 }
 
 module.exports = nextConfig
+
